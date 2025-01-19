1e3855640dd88f238fa75a75373edaaba9cad736
JoshuaDoes <joshuadoespersonalstuff@gmail.com>
2025-01-19 16:27:04 -0500

    hapticify: Support some arguments

--- go.mod
+++ go.mod
@@ -2,6 +2,8 @@ module github.com/JoshuaDoes/hapticify
 
 go 1.23.4
 
+require github.com/spf13/pflag v1.0.5
+
 require (
 	github.com/JoshuaDoes/crunchio v0.0.0-20250119012423-b0cceb64328c // indirect
 	github.com/superwhiskers/crunch/v3 v3.5.7 // indirect
--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"github.com/spf13/pflag"
+
 	"fmt"
-	"os"
 	"runtime"
 	"time"
 )
@@ -11,10 +12,33 @@ var (
 	ErrorAlreadyRunning error = fmt.Errorf("ffmpeg: already running")
 )
 
+var (
+	in, out		string
+	high, low	int
+	vol, hap	float64
+)
+
 func main() {
+	pflag.CommandLine.SortFlags = false
+	pflag.StringVarP(&in, "in", "i", "", "input audio")
+	pflag.StringVarP(&out, "out", "o", "", "output audio")
+	pflag.IntVarP(&high, "high", "h", 70, "highpass freq")
+        pflag.IntVarP(&low, "low", "l", 150, "lowpass freq")
+        pflag.Float64VarP(&vol, "vol", "v", 1, "audio dB gain")
+        pflag.Float64VarP(&vol, "hap", "s", 1, "haptics dB gain")
+	pflag.Parse()
+
+	if in == "" {
+		panic("need input")
+	}
+	if out == "" {
+		out = in + ".hapticify.ogg"
+	}
+
 	ff := NewFFmpeg("libvorbis", "ogg")         //Android haptics must be OGG Vorbis
-	ff.SetInput(os.Args[1])                     //Input file
-	ff.SetOutput(os.Args[2] + ".hapticify.ogg") //Output file
+	ff.SetInput(in)                             //Input file
+	ff.SetOutput(out)                           //Output file
+	ff.SetMetadata("HAPTICIFY", "JoshuaDoes")   //:D
 	ff.SetMetadata("ANDROID_HAPTIC", "2")       //Tell Android we have stereo haptics
 	ff.SetInputChannels(2)                      //Mix input to stereo
 	ff.SetOutputChannels(4)                     //Mix output to stereo audio + stereo haptics
@@ -27,17 +51,14 @@ func main() {
 	hp := ff.NewFilter()
 	hp.SetInputChannels(0, 1)
 	hp.SetOutputChannels(0, 1)
-	hp.Add(NewFilterHighpass(70))
+	hp.Add(NewFilterHighpass(high))
+	hp.Add(NewFilterVolumeGain(vol))
 
 	lp := ff.NewFilter()
 	lp.SetInputChannels(0, 1)
 	lp.SetOutputChannels(2, 3)
-	lp.Add(NewFilterLowpass(150))
-	lp.Add(NewFilterEqualizer(10, 20, 5.0))
-	lp.Add(NewFilterEqualizer(30, 20, 4.5))
-	lp.Add(NewFilterEqualizer(50, 20, 4.0))
-	lp.Add(NewFilterEqualizer(65, 10, 3.0))
-	lp.Add(NewFilterEqualizer(110, 80, 2.0))
+	lp.Add(NewFilterLowpass(low))
+	lp.Add(NewFilterVolumeGain(hap))
 
 	//Start the conversion process
 	if err := ff.Start(); err != nil {
